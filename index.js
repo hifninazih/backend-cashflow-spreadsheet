51506a59ad30308e13daecf2dc196fa5ea92bb6d
Hifnie Nazih <hifninazih111@gmail.com>
2024-01-14 15:09:30 +0700

    add vercel config

---
+++ index.js
@@ -0,0 +1,34 @@
+// Load Express
+const express = require("express");
+// Load path
+const path = require("path");
+// Create Express App
+const app = express();
+
+const axios = require("axios");
+
+app.use(express.json()); // JSON-encoded Middleware
+app.use(express.urlencoded({ extended: true })); // URL-encoded Middleware untuk parsing body
+
+app.post("/", (req, res) => {
+  axios
+    .post(
+      "https://script.google.com/macros/s/AKfycbxfQwmbySGrUF7K31M5Fo0K0j_RuUY5V0oLm3wb487psrBFBCRUPZbesGBo8RvJxXv9Pg/exec",
+      req.body
+    )
+    .then((response) => {
+      const data = response.data;
+      res.send(data);
+    })
+    .catch((error) => {
+      res.send(error);
+    });
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
